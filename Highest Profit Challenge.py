bef04473c2ca0045b27d304ac713d065fc3171a5
jmbunwe <64099106+jmbunwe@users.noreply.github.com>
2021-04-09 11:49:49 -0400

    Update Highest Profit Challenge.py

--- Highest Profit Challenge.py
+++ Highest Profit Challenge.py
@@ -1,62 +1,41 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
+# importing the necessary libraries
 import pandas as pd
 import json
 
 
-# # Part 1
-
-# In[60]:
+# Part 1
 
 
-df = pd.read_csv("data.csv")
-df_copy = df #making a copy 
+df = pd.read_csv("data.csv") # reading in the dataframe
+df_copy = df # making a copy of the dataframe so we don't overwrite the data
 
-print(len(df_copy))
-
-
-# In[61]:
+print(len(df_copy)) # prints the number of rows in the original dataframe
 
 
-df_copy["Profit (in millions)"].tolist() #did a quick check found that non-numeric was NA
-numeric_filter = df_copy["Profit (in millions)"] != "N.A." 
-df_copy = df_copy[numeric_filter]
+df_copy["Profit (in millions)"].tolist() # did a quick visual check and found that the non-numeric value was the string N.A.
+numeric_filter = df_copy["Profit (in millions)"] != "N.A." # made a mask that only pulled the rows without the N.A. string
+df_copy = df_copy[numeric_filter] # applied the mask so that the dataframe only contained the rows with numeric values
 
-print(len(df_copy))
-
-
-# In[4]:
+print(len(df_copy)) # prints the number of rows of the modified dataframe without the non-numeric values
 
 
-df_copy.head()
+# Part 2
 
 
-# # Part 2
-
-# In[19]:
-
-
+# following process takes the string values in the "profit" column and converts each value to a float
 for num in df_copy["Profit (in millions)"]:
     df_copy["Profit (in millions)"].replace(to_replace = num, value = float(num))
 
 
-# In[45]:
-
-
-#making JSON
-# data2 = df_copy.to_json()
+# making JSON file of our updated dataframe
 df_copy.to_json(r'C:\Users\woods\Documents\GCP Challenge\data2.json')
 
 
-# In[59]:
-
-
+# sort the dataframe by the "profit" values in descending order (highest to lowest)
 df_copy.sort_values(by=["Profit (in millions)"], inplace=True, ascending = False)
 
+
+# will print the top 20 rows corresponding to the sorted "profit" column 
 for i in range(0,20):
     print(df_copy.iloc[i,:])
 
